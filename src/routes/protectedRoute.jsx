8372fb49a9b392216cb787c24b36aa79b129aa18
unknown <netstantechnology@gmail.com>
2024-05-29 20:53:40 +0100

    Redirect Fix

--- src/routes/protectedRoute.jsx
+++ src/routes/protectedRoute.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+import { useAuth } from "context/AuthContext";
 
 const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem("accessToken");
+  const { logout } = useAuth();
 
   if (!token) {
-    return <Navigate to="/login" replace />;
+    return logout(), (<Navigate to="/login" replace />);
   }
 
   return children;
